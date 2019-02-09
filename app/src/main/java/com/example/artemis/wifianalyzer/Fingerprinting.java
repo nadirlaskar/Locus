160b2d73166385b5ceaccdd488a44d10e3e36bd4
nadir <nadirlaskar@gmail.com>
2019-02-10 03:38:26 +0530

    added circular progress for finger printing added data to spot spinner

--- app/src/main/java/com/example/artemis/wifianalyzer/Fingerprinting.java
+++ app/src/main/java/com/example/artemis/wifianalyzer/Fingerprinting.java
@@ -4,19 +4,25 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -26,6 +32,10 @@ import java.util.TimerTask;
 
 public class Fingerprinting extends AppCompatActivity {
 
+    Snackbar status;
+    Timer P;
+    FloatingActionButton fab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,24 +43,96 @@ public class Fingerprinting extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         wifiManager = (WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if(status==null || !status.isShown()){
+                    status = showSnackbarWithProgressBar(Fingerprinting.this,view);
+                    ((FloatingActionButton) view).setImageDrawable(ContextCompat.getDrawable(Fingerprinting.this,R.drawable.stop));
+                    status.show();
+                }
+                else {
+                    resetProgressState();
+                }
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+          ActionBar ab = getSupportActionBar();
+          if(ab!=null)
+                ab.setDisplayHomeAsUpEnabled(true);
 
         addSignalItems();
-
+        addSpots();
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
         getApplicationContext().registerReceiver(wifiScanReceiver, intentFilter);
         if(!wifiManager.isWifiEnabled()){
             wifiManager.setWifiEnabled(true);
         }
+    }
+
+    private void addSpots() {
+        List<String> spinnerArray = new ArrayList<>();
+        spinnerArray.add("ODC-9-Corridor");
+        spinnerArray.add("ODC-9-Center");
+        spinnerArray.add("Gemini");
+        spinnerArray.add("6th Floor Washroom Men's");
+        spinnerArray.add("6th Floor Washroom Women's");
+        spinnerArray.add("6th Floor Lift Lobby");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, R.layout.spinner_spot_list, spinnerArray);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner sItems = (Spinner) findViewById(R.id.spinner);
+        if(sItems!=null)
+         sItems.setAdapter(adapter);
+    }
+
+    Snackbar showSnackbarWithProgressBar(Context context,View view){
+        Snackbar bar = Snackbar.make(view, "Capturing fingerprints...", Snackbar.LENGTH_INDEFINITE);
+        Snackbar.SnackbarLayout snack_view = (Snackbar.SnackbarLayout) bar.getView();
+        ProgressBar pbar =  new ProgressBar(context,null,android.R.attr.progressBarStyleHorizontal);
+        Resources res = getResources();
+        Drawable drawable = res.getDrawable(R.drawable.circular_progress);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        pbar.setLayoutParams(params );
+        pbar.setProgress(0);   // Main Progress
+        pbar.setSecondaryProgress(100); // Secondary Progress
+        pbar.setMax(100); // Maximum Progress
+        pbar.setProgressDrawable(drawable);
+        updateProgressBar(pbar);
+        snack_view.addView(pbar);
+        return bar;
+    }
+
+
+    void updateProgressBar(final ProgressBar pb){
+        P = new Timer();
+        final int[] i = {0};
+        P.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        pb.setProgress(i[0]++);
+                        if(i[0]>=100){
+                            resetProgressState();
+                        }
+                    }
+                });
+            }
+        }, 0, 100);
+    }
+
+    private void resetProgressState() {
+        P.cancel();
+        status.dismiss();
+        P.purge();
+        fab.setImageDrawable(ContextCompat.getDrawable(Fingerprinting.this,R.drawable.record));
     }
 
     Timer T,T1;
@@ -139,8 +221,8 @@ public class Fingerprinting extends AppCompatActivity {
                                 signals.add(new Signal(img, SSID,dbm+" dBm"));
                             }
                         }
-
-                        signalListView.setAdapter(new SignalAdapter(Fingerprinting.this, signals));
+                        if(signalListView!=null)
+                          signalListView.setAdapter(new SignalAdapter(Fingerprinting.this, signals));
                     }
                 });
             }
